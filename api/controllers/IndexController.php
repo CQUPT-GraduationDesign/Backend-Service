fa9f7e2753b08f75540ff6cb49e05bae3273810d
lk1ngaa7 <lk1ngaa7@gmail.com>
2016-05-14 15:55:53 +0800

    test migrate

--- api/controllers/IndexController.php
+++ api/controllers/IndexController.php
@@ -4,6 +4,7 @@ use Yii;
 use yii\web\Controller;
 
 class IndexController extends Controller {
+
     public function actionError(){
         $exception = Yii::$app->errorHandler->exception;
         var_dump($exception);
